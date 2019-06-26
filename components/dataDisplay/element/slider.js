73e8384dcd426c1bc8a87e0ff13e30ab3ba4f1ce
kacoulib <kacoulib@gmail.com>
2019-06-26 13:41:14 +0200

    Added slider component

---
+++ components/dataDisplay/element/slider.js
@@ -0,0 +1,21 @@
+import Slick from "react-slick";
+
+const defaultSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+};
+
+const Slider = (props) => {
+    const settings = Object.assign(defaultSettings, { ...props.settings });
+
+    return (
+        <Slick {...settings}>
+            {props.children}
+        </Slick>
+    )
+}
+
+export default Slider;
