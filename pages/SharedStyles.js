e14f615b52471d0da8a2b836999a6e0634b71ccb
Loic CHETY <lchety@e2r12p11.42.fr>
2019-06-26 13:38:17 +0200

    wip

--- pages/SharedStyles.js
+++ pages/SharedStyles.js
@@ -3,7 +3,12 @@ const videoOverlay = {
 	zIndex: '5',
 }
 
+const indexContainer = {
+	padding: '40px',
+	color: '#232941',
+}
 
 module.exports = {
 	videoOverlay,
+	indexContainer,
 };
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import styles from './SharedStyles';
 
 
 import withLayout from '../lib/withLayout';
+import Typography from '@material-ui/core/Typography';
 
 const Index = () => (
 	<div style={{ padding: '10px 45px' }}>
@@ -27,6 +28,79 @@ const Index = () => (
 				<p>Content above your video</p>
 				</div>
 			</video>
+		</Grid>
+		<Grid container>
+			<Grid item align="center" style={{padding: '40px'}} xs={12} sm={12}>
+				<Typography variant="h4" gutterBottom style={{color: '#232941'}}>
+					Comment rendons-nous l'influence marketing scalable
+				</Typography>
+			</Grid>
+			<Grid item align="center" style={{padding: '40px'}} xs={6} sm={6}>
+				<img src="https://via.placeholder.com/400x250" />
+			</Grid>
+			<Grid item align="center" alignItems="center" style={{padding: '40px'}} xs={6} sm={6}>
+				<Typography variant="h5" gutterBottom style={{color: '#232941'}}>
+					Stratégie de contenu
+				</Typography>
+				<Typography variant="h5" gutterBottom style={{color: '#232941'}}>
+					Lignes éditoriales
+				</Typography>
+				<Typography variant="h5" gutterBottom style={{color: '#232941'}}>
+					Stratégie de plateforme
+				</Typography>
+				<Typography variant="h5" gutterBottom style={{color: '#232941'}}>
+					Production
+				</Typography>
+				<Typography variant="h5" gutterBottom style={{color: '#232941'}}>
+					Edition
+				</Typography>
+			</Grid>
+		</Grid>
+		<Grid container style={{backgroundColor: "#DDD"}}>
+			<Grid item align="center" style={{padding: '40px'}} xs={12} sm={12}>
+				<Typography variant="h4" gutterBottom style={{color: '#232941'}}>
+					Data et Technologie
+				</Typography>
+			</Grid>
+			<Grid item align="center" alignItems="center" style={{padding: '40px'}} xs={6} sm={6}>
+				<Typography variant="h5" gutterBottom style={{color: '#232941'}}>
+					Search influenceurs
+				</Typography>
+				<Typography variant="h5" gutterBottom style={{color: '#232941'}}>
+					Prévention de la fraude
+				</Typography>
+				<Typography variant="h5" gutterBottom style={{color: '#232941'}}>
+					Affinité influenceurs/marque
+				</Typography>
+				<Typography variant="h5" gutterBottom style={{color: '#232941'}}>
+					Audience et analytics
+				</Typography>
+			</Grid>
+			<Grid item align="center" style={{padding: '40px'}} xs={6} sm={6}>
+				<img src="https://via.placeholder.com/400x250" />
+			</Grid>
+		</Grid>
+		<Grid container>
+			<Grid item align="center" style={{padding: '40px'}} xs={6} sm={6}>
+				<img src="https://via.placeholder.com/400x250" />
+			</Grid>
+			<Grid item align="center" alignItems="center" style={{padding: '40px'}} xs={6} sm={6}>
+				<Typography variant="h5" gutterBottom style={{color: '#232941'}}>
+					Managment et optimization
+				</Typography>
+				<Typography variant="h5" gutterBottom style={{color: '#232941'}}>
+					Relation influenceurs
+				</Typography>
+				<Typography variant="h5" gutterBottom style={{color: '#232941'}}>
+					Account managment
+				</Typography>
+				<Typography variant="h5" gutterBottom style={{color: '#232941'}}>
+					Qualité de service
+				</Typography>
+				<Typography variant="h5" gutterBottom style={{color: '#232941'}}>
+					Logistique
+				</Typography>
+			</Grid>
 		</Grid>
 	</Grid>
 
