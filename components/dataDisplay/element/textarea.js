34b78cd2b8d2698f01d0dc76030cb21e4256c0cd
kacoulib <kacoulib@gmail.com>
2019-07-25 11:24:54 +0200

    Added upload component

---
+++ components/dataDisplay/element/textarea.js
@@ -0,0 +1,13 @@
+const TextAreaCom = ({ }) => (
+    <TextField
+        id="outlined-textarea"
+        label="Multiline Placeholder"
+        placeholder="Placeholder"
+        multiline
+        className={classes.textField}
+        margin="normal"
+        variant="outlined"
+    />
+)
+
+export default TextAreaCom
---
+++ components/page/marques/contact-info.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from "react";
+import Grid from '@material-ui/core/Grid';
+import PropTypes from 'prop-types'
+import FormGenerator from '../../form/generator'
+import { buttonStyle } from '../../../utils/variables/css';
+import Button from '@material-ui/core/Button';
+import { choiceList } from '../../../utils/variables/general'
+
+const cardContainer = {
+    padding: '1rem',
+    color: 'white',
+    backgroundColor: '#242B45',
+    margin: '.5em'
+}
+const styles = {
+    cardContainer,
+    rightCardContainer: Object.assign({}, cardContainer, { backgroundColor: '#F13F4B' }),
+    container: { padding: '0 3em' },
+    cardText: { margin: '.5em 0 0' },
+    status: { color: '#F13F4B' },
+    stars: { width: '50%' },
+    influencer_img_container: { height: 'auto', paddingTop: 5 },
+    influencer_info_container: { padding: '1rem' },
+}
+
+const fields = [
+    {
+        label: "Choix de la page marque",
+        name: "child",
+        type: 'select',
+        required: true,
+        width: 6,
+        props: {
+            list: choiceList,
+            margin: "normal",
+            variant: "outlined",
+            required: true
+        }
+    },
+    {
+        label: "Image / Logo",
+        name: "picture",
+        type: 'upload',
+        required: true,
+        width: 12,
+        props: {
+        }
+    },
+    {
+        label: "Bio",
+        name: "phone",
+        type: 'textarea',
+        required: true,
+        width: 12,
+        props: {
+            list: choiceList,
+            required: true,
+            // multiline: true,
+            margin: "normal",
+            variant: "outlined",
+            rows: 15
+        }
+    },
+]
+
+const Index = ({ selectedInfluencer }) => {
+
+    const [form, setForm] = useState({})
+
+    const onChange = (name, value) => {
+        console.log(name, value)
+        setForm(Object.assign({}, form, { [name]: value }))
+    };
+    const onSubmit = async () => {
+        console.log(form)
+    };
+
+
+    return (
+        <Grid container alignItems='center' justify="center" style={styles.container} >
+            <Grid item xs={12} sm={12}>
+                <h2>Information de contact</h2>
+            </Grid>
+
+            <Grid item xs={12} sm={12}>
+
+                <FormGenerator
+                    fields={fields}
+                    form={form}
+                    onChange={onChange}
+                    onSubmit={onSubmit}
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <Button variant="contained" type="submit" style={buttonStyle} onClick={() => onSubmit()}>
+                    Submit
+                </Button>
+            </Grid>
+        </ Grid>
+    )
+}
+Index.propTypes = {
+    selectedInfluencer: PropTypes.object.isRequired,
+}
+
+export default Index;
