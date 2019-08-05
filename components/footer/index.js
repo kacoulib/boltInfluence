eb1cc06f5d536ee5cf4781f454201cb150a66cb6
kacoulib <kacoulib@gmail.com>
2019-08-05 11:00:17 +0200

    Fixed responsive footer display

--- components/footer/index.js
+++ components/footer/index.js
@@ -9,25 +9,25 @@ import Socials from '../../components/elements/socials'
 const Footer = () => (
 	<footer id='footer' className='card-bg dots-white bottom'>
 		<Grid container alignItems="center" justify="center" align='center'>
-			<Grid item sm={12}>
+			<Grid item sm={12} xs={12}>
 				<Link href='/'><a title='home logo'><img src={logo} /></a></Link>
 			</Grid>
-			<Grid item sm={4}>
+			<Grid item sm={4} xs={6}>
 				<ul>
 					<li><Link href='/annonceurs'><a title='Annonceurs'>Annonceurs</a></Link></li>
 					<li><Link href='/influenceurs'><a title='Influenceurs'>Influenceurs</a></Link></li>
 					<li><Link href="/about">La vidéo d'influence</Link></li>
 				</ul>
 			</Grid>
-			<Grid item sm={4}>
+			<Grid item sm={4} xs={6}>
 				<ul>
 					<li><Link href="/about"><a title='Notre méthode'>Notre méthode</a></Link></li>
 					<li><Link href="/contact"><a title='contact'>Contactez-nous</a></Link></li>
 				</ul>
 			</Grid>
-			<Grid item sm={4}>
+			<Grid item sm={4} xs={12}>
 				<ul>
-					<li><Link href="/about"><a title='Suivez noous'>Suivez noous</a></Link></li>
+					<li><Link href="/about"><a title='Suivez noous'>Suivez nous</a></Link></li>
 					<li>
 						<Socials />
 					</li>
