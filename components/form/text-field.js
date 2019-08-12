4ed2e66b8b95831df41fa759a8e5aa3021f715a3
kacoulib <kacoulib@gmail.com>
2019-08-12 12:40:55 +0200

    Added form text-field

--- components/form/text-field.js
+++ components/form/text-field.js
@@ -1,31 +1,55 @@
-import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
+import PropTypes from 'prop-types'
+import { FormElementWrapper } from './index'
+import { makeStyles } from '@material-ui/styles';
 
-const TextFieldComp = ({ name, type, value, dimension, label, disableUnderline, onChange, showLabel, elemProps }) => {
-    console.log(dimension)
+
+const useStyles = makeStyles(theme => ({
+    textField: {
+        [`& fieldset`]: {
+            border: 0,
+            boxShadow: '0px 2px 5px #bfbfbf',
+        },
+    }
+}))
+
+const TextFieldComp = ({ name, label, type, value, onChange, showLabel, unableUnderline, unableBoxShadow = true }) => {
+    const classes = useStyles();
+    const variant = unableBoxShadow ? "outlined" : "standard"
+
     return (
-        <Grid container>
-            {showLabel && <Grid item {...labelSpacing}>{label}</Grid>}
-            <Grid>
-                <Grid item {...dimension}>
-                    <TextField
-                        type={type}
-                        label={showLabel ? '' : label}
-                        multiline={type === 'textarea'}
-                        fullWidth
-                        value={value}
-                        onChange={onChange(name)}
-                        InputProps={{
-                            disableUnderline: !!disableUnderline
-                        }}
-                        style={{ paddingRight: '15px' }}
-                        {...elemProps}
-                        rows={8}
-                        rowsMax={10}
-                    />
-                </Grid>
-            </Grid>
-        </Grid>
+        <FormElementWrapper label={label} showLabel={showLabel}>
+            <TextField
+                multiple
+                type={type}
+                label={showLabel ? '' : label}
+                multiline={type === 'textarea'}
+                fullWidth
+                value={value}
+                onChange={onChange(name)}
+                InputProps={{
+                    disableUnderline: !!unableUnderline,
+                }}
+                className={classes.textField}
+                style={{ width: '100%' }}
+                rows={8}
+                variant={variant}
+                rowsMax={10}
+            />
+        </FormElementWrapper>
     )
+}
+
+
+TextFieldComp.propTypes = {
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string,
+    type: PropTypes.string.isRequired,
+    value: PropTypes.any.isRequired,
+    onChange: PropTypes.func.isRequired,
+    dimension: PropTypes.object,
+    showLabel: PropTypes.bool,
+    unableUnderline: PropTypes.bool,
+    unableBoxShadow: PropTypes.bool,
 }
 export default TextFieldComp
