0b99d1447a8e16cee6ef2783d32cdeeb400bdd9c
kacoulib <kacoulib@gmail.com>
2019-08-14 15:51:35 +0200

    Added Social btn

--- components/elements/socialBtn.js
+++ components/elements/socialBtn.js
@@ -2,8 +2,8 @@ import { Grid } from '@material-ui/core';
 import { Link } from '../../server/routes/next-routes'
 
 const SocialBtnComp = ({ type = 'instagram', href = '#', text = 'Log in', onClick }) => (
-    <div className={`btn-icon icon-${type}`} id='toto'>
-        <Link variant="contained" href="/auth/google" >
+    <div className={`btn-icon icon-${type}`} onClick={onClick}>
+        <Link variant="contained" href={href} >
             <Grid container justify="center" alignItems="center" className='text-center'>
                 <Grid item xs={2} sm={2}>
 
