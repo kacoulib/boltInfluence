2b94f495b15822f27a5dfab8daf90b3689cc09c6
kacoulib <kacoulib@gmail.com>
2019-08-23 17:47:31 +0200

    Added linkedin link to socials

--- components/elements/socials.js
+++ components/elements/socials.js
@@ -8,7 +8,7 @@ const SocialsComp = ({ parent, child, fill, stroke }) => (
     <ul id='socials' style={{ ...parent }}>
         <li className='inline-block' style={{ ...child }}><Link href='https://www.facebook.com/boltinfluence/'><a target='_blank' title='Bolt social Facebook'><Facebook fill={fill} stroke={stroke} /></a></Link></li>
         <li className='inline-block' style={{ ...child }}><Link href='https://twitter.com/BoltInfluence'><a target='_blank' title='Bolt social Twitter'><Twitter fill={fill} stroke={stroke} /></a></Link></li>
-        <li className='inline-block' style={{ ...child }}><Link href='#'><a target='_blank' title='Bolt social Linkedin'><Linkedin fill={fill} stroke={stroke} /></a></Link></li>
+        <li className='inline-block' style={{ ...child }}><Link href='https://www.linkedin.com/company/bolt-influence/'><a target='_blank' title='Bolt social Linkedin'><Linkedin fill={fill} stroke={stroke} /></a></Link></li>
         <li className='inline-block' style={{ ...child }}><Link href='https://www.instagram.com/boltinfluence/'><a target='_blank' title='Bolt social Instagram'><Instagram fill={fill} stroke={stroke} /></a></Link></li>
     </ul>
 )
