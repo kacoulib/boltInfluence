df0a617adac9f707d3e1d12090f23de61fff079e
kacoulib <kacoulib@gmail.com>
2019-09-01 02:14:08 +0200

    Separate message logic form display

--- components/dataDisplay/message/index.js
+++ components/dataDisplay/message/index.js
@@ -3,10 +3,93 @@ import PropTypes from 'prop-types'
 import Grid from '@material-ui/core/Grid';
 import TextTruncate from 'react-text-truncate';
 
+const MessageDisplay = ({ selected = null, messages = [], offset = 0, limit = 0, nb_message = 0, handleSelection, handlePaginate }) => (
+    <Grid container id="tchat" alignItems='flex-start'>
+        <Grid item container sm={4} justify="space-between" direction="column" className='fullheight'>
+            <Grid item container className='tchat-list-container'>
+                {messages && messages.map((elem, index) => (
+                    <Grid item container key={index} sm={12} className={`tchat-list ${elem.status} ${selected._id == elem._id ? 'selected' : ''}`} onClick={() => handleSelection('selected_id', elem._id)}>
+                        <Grid item container alignItems='center'>
+                            <Grid item container sm={8}>
+                                <h2>{`${elem.user.firstName} ${elem.user.lastName}`}</h2>
+                            </Grid>
+                            <Grid item sm={4}>
+                                <div className='message-date'>{elem.date}</div>
+                            </Grid>
+                        </Grid>
+                        <Grid>
+                            <div className='message-trucate'>
+                                <TextTruncate
+                                    line={2}
+                                    element="div"
+                                    truncateText="…"
+                                    text={elem.content}
+                                />
+                            </div>
+                        </Grid>
+                    </Grid>
+                ))}
+            </Grid>
+            <Grid item container alignItems="center" justify="flex-end" className='paginate'>
+                <Grid item >
+                    <span>{`${offset}-${limit}`}</span> de<span> {nb_message}</span>
+                </Grid>
+                <Grid item direction="row">
+                    <div className='paginate-btn' onClick={() => handlePaginate(offset - limit)}>x</div>
+                    <div className='paginate-btn' onClick={() => handlePaginate(offset + limit)}>></div>
+                </Grid>
+            </Grid>
+        </Grid>
+        <Grid item sm={8}>
+            <section>
+                {selected && <>
+                    <Grid container>
+                        <Grid item container alignItems='center' className='tchat-header'>
+                            <Grid item container alignItems='center' sm={8}>
+                                {selected.user && <>
+                                    <Grid item>
+                                        <img src={selected.user.img} />
+                                    </Grid>
+                                    <Grid item>
+                                        <div className='header-user-name'>
+                                            <h2>{`${selected.user.firstName} ${selected.user.lastName}`}</h2>
+                                            <h3>{selected.user.slug}</h3>
+                                        </div>
+                                    </Grid>
+                                </>}
+                            </Grid>
+                            <Grid item sm={4}>
+                                <div className='message-date'>{selected.date}</div>
+                            </Grid>
+                        </Grid>
+                        <Grid item>
+                            <div>
+                                <h1>{selected.title}</h1>
+                                <p>{selected.content}</p>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </>}
+            </section>
+        </Grid>
+        <style jsx>{`
+                h2, h3 {
+                    font-size: 1rem;
+                    margin: 0;
+                }
+                .message-date {
+                    font-size: .8rem;
+                }
+                .header-user-name {
+                    padding-left: 1rem;
+                }
+            `}</style>
+    </Grid>
+)
 
-const MessageComp = ({ paginate }) => {
+const MessageComp = () => {
     const [state, setState] = useState({
-        selected: 1,
+        selected_id: 1,
         offset: 1,
         limit: 50,
         nb_message: 80,
@@ -111,88 +194,16 @@ const MessageComp = ({ paginate }) => {
 
         onChange('offset', offset);
     }
-    const selected = state.messages.find(e => e._id == state.selected);
+    const selected = state.messages.find(e => e._id == state.selected_id);
 
     return (
-        <Grid container id="tchat" alignItems='flex-start'>
-            <Grid item container sm={4} justify="space-between" direction="column" className='fullheight'>
-                <Grid item container className='tchat-list-container'>
-                    {state.messages && state.messages.map((elem, index) => (
-                        <Grid item container key={index} sm={12} className={`tchat-list ${elem.status} ${selected._id == elem._id ? 'selected' : ''}`} onClick={() => handleSelection('selected', elem._id)}>
-                            <Grid item container alignItems='center'>
-                                <Grid item container sm={8}>
-                                    <h2>{`${elem.user.firstName} ${elem.user.lastName}`}</h2>
-                                </Grid>
-                                <Grid item sm={4}>
-                                    <div className='message-date'>{elem.date}</div>
-                                </Grid>
-                            </Grid>
-                            <Grid>
-                                <div className='message-trucate'>
-                                    <TextTruncate
-                                        line={2}
-                                        element="p"
-                                        truncateText="…"
-                                        text={elem.content}
-                                    />
-                                </div>
-                            </Grid>
-                        </Grid>
-                    ))}
-                </Grid>
-                <Grid item container alignItems="center" justify="flex-end" className='paginate'>
-                    <Grid item >
-                        <span>{`${state.offset}-${state.limit}`}</span> de<span> {state.nb_message}</span>
-                    </Grid>
-                    <Grid item direction="row">
-                        <div className='paginate-btn' onClick={() => handlePaginate(state.offset - state.limit)}>x</div>
-                        <div className='paginate-btn' onClick={() => handlePaginate(state.offset + state.limit)}>></div>
-                    </Grid>
-                </Grid>
-            </Grid>
-            <Grid item sm={8}>
-                <section>
-                    {selected && <>
-                        <Grid container>
-                            <Grid item container alignItems='center' className='tchat-header'>
-                                <Grid item container alignItems='center' sm={8}>
-                                    <Grid item>
-                                        <img src={selected.user.img} />
-                                    </Grid>
-                                    <Grid item>
-                                        <div className='header-user-name'>
-                                            <h2>{`${selected.user.firstName} ${selected.user.lastName}`}</h2>
-                                            <h3>{selected.user.slug}</h3>
-                                        </div>
-                                    </Grid>
-                                </Grid>
-                                <Grid item sm={4}>
-                                    <div className='message-date'>{selected.date}</div>
-                                </Grid>
-                            </Grid>
-                            <Grid item>
-                                <div>
-                                    <h1>{selected.title}</h1>
-                                    <p>{selected.content}</p>
-                                </div>
-                            </Grid>
-                        </Grid>
-                    </>}
-                </section>
-            </Grid>
-            <style jsx>{`
-                h2, h3 {
-                    font-size: 1rem;
-                    margin: 0;
-                }
-                .message-date {
-                    font-size: .8rem;
-                }
-                .header-user-name {
-                    padding-left: 1rem;
-                }
-            `}</style>
-        </Grid>
+        <MessageDisplay
+            selected={selected}
+            handlePaginate={handlePaginate}
+            handleSelection={handleSelection}
+            handlePaginate={handlePaginate}
+            {...state}
+        />
     )
 }
 
